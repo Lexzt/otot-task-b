b1691a641f5c3d9e82fe58b11220cae7a07bbd03
Lexzt <fongzhizhong@gmail.com>
2020-10-02 23:49:51 +0800

    idk what I am doing anymore

--- main.test.js
+++ main.test.js
@@ -1,6 +1,6 @@
 const supertest = require("supertest");
 const http = require("http");
-const app = require("./test/app.js");
+const app = require("./app.js");
 
 describe("Get Endpoints", () => {
   let server;
